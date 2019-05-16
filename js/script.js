9b67cbaad7edd8651b6718804d8770e867ee6954
upoma8901 <UPOMAB8901@QUEENSVOC.ORG>
2019-05-16 22:30:05 +0000

    ms

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
-/* global $ */
+$('#searchButton').click(function() {
+        var searchTerm=$("#searchTerm").val();
+        var Url="https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s="+searchTerm;
+
+
+        $.ajax({
+          url: Url,
+          method: "GET",
+          success: function(response) {
+              var search=[];
+
+              var Src=response.Search
+              for (i = 0; i < Src; i++) {
+              var imageSrc = Src[i].Poster;
+              // imageHTtml = "//.png"
+              var imageHtml="<img src="+imageSrc+">";
+              var titleSrc=response.Search[i].Title;
+                   var titleHtml="<p>"+titleSrc+"</p>";
+                   var yearSrc=response.Search[i].year;
+                   var yearHtml="<p>"+yearSrc+"</p>";
+}
+
+
+                             console.log(imageSrc);
+
+
+
+
+
+              $("body").append(imageHtml);
+          },
+        });
+    });
+    /* global $ */
 
 console.log('hi');
 
